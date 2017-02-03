0fd6c22a865153b88b2f511fdf41d7fd3b0d9ae5
blazejp <przybysz.b@gmail.com>
2017-02-03 09:31:31 +0100

    main script

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { data } from "./helper";
+
+const container = document.querySelector(".container");
+
+const data = {"element": "element"};
+
+const onInit = function() {
+   
+    container.appendChild(document.createTextNode("kontener")); 
+};
+
+onInit();
